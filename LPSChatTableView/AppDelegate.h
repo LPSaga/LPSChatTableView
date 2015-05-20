2ceb2ff8e3a7c2d94fad87c12b2db4e0ba898c68
AnillusionXIII <lpsaga10@163.com>
2015-05-20 16:55:27 +0800

    Initial Commit

---
+++ LPSChatTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LPSChatTableView
+//
+//  Created by liupei on 15/5/20.
+//  Copyright (c) 2015年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LPSChatTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LPSChatTableView
+//
+//  Created by liupei on 15/5/20.
+//  Copyright (c) 2015年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
